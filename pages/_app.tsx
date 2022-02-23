88e96c82fb082e93d05c35a0feffa1d7528a6d5e
Maxim Buz <mbuz.maxim@gmail.com>
2022-02-23 12:11:25 +0100

    set up project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+/* REACT */
+import { useState } from 'react'
+
+/* REACT QUERY */
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+       <Hydrate state={pageProps.dehydratedState}>
+         <Component {...pageProps} />
+       </Hydrate>
+     </QueryClientProvider>
+  )
 }
 
 export default MyApp
