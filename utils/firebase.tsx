a46556836f64e3cf6223c861926574783849a1ba
Maxim Buz <mbuz.maxim@gmail.com>
2022-02-23 12:11:34 +0100

    included firebase stuff

---
+++ utils/firebase.tsx
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+import 'firebase/auth';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZ7EKgkA7PQPFLcVSoF4nSOBMdWcj-ZVU",
+  authDomain: "kuvats-2e148.firebaseapp.com",
+  projectId: "kuvats-2e148",
+  storageBucket: "kuvats-2e148.appspot.com",
+  messagingSenderId: "149676819028",
+  appId: "1:149676819028:web:2dbd81db2878cfaf0d63c1",
+  measurementId: "G-4L925JZPPY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+// Exporting
+export const auth = getAuth();
+export const firestore = getFirestore(app);
+export default app;
